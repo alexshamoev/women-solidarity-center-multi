6fddda175e69b1cf00d38bf130b67c703cc4d884
Nika_Kvaratskhelia <barca10111@gmail.com>
2023-06-09 18:38:14 +0400

    cache clear

---
+++ app/Http/Middleware/ImageCacheClean.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ImageCacheClean
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        
+        if ($response->getStatusCode() == 200 ) {
+
+            $content = $response->getContent();
+            
+            $content = preg_replace('/(src="[^"]+\/storage\/images\/[^"]+\.(jpe?g|png)\/?[^"]*)"/', '$1?v=' . time() . '"', $content);
+
+
+            $response->setContent($content);
+        }
+
+        return $response;
+    }
+}
